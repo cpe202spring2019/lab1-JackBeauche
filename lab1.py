3841e01980ab09f5d237bfd1f4e1bb5f5d4de802
Jack Beauchesne <jackbeauchesne16@gmail.com>
2019-04-05 11:01:42 -0700

    Added recursive functions and comments.

--- lab1.py
+++ lab1.py
@@ -14,7 +14,7 @@ def max_list_iter(int_list):  # must use iteration not recursion
         MaxVal = int_list[0] # Assume MaxVal is first value
         
         for i in int_list:
-            if i > MaxVal:
+            if i > MaxVal: # Update MaxVal if an item is larger
                 MaxVal = i
         return(MaxVal)				
     pass
@@ -22,12 +22,32 @@ def max_list_iter(int_list):  # must use iteration not recursion
 def reverse_rec(int_list):   # must use recursion
 #   """recursively reverses a list of numbers and returns the reversed list
 #   If list is None, raises ValueError"""
-   pass
+    if(type(int_list) == type(None)):
+        raise ValueError
+    elif(len(int_list) == 0):
+        return(int_list)
+    else:
+        return(reverse_rec(int_list[1:]) + int_list[0:1])   
+    pass
 
 def bin_search(target, low, high, int_list):  # must use recursion
 #   """searches for target in int_list[low..high] and returns index if found
 #   If target is not found returns None. If list is None, raises ValueError """
-   pass
+    if(type(int_list) == type(None)):
+        raise ValueError
+    elif( low <= high):
+    
+        mid = (low + high)//2 # mid point is defined
+         
+        if(target == int_list[mid]): # return mid if target is found
+            return(mid)
+        elif(target < int_list[mid]): # can search only the lower half of the list, since target is not found, we decrease 1 from mid and that is new high
+            return(bin_search(target, low, mid - 1, int_list))
+        else:
+            return(bin_search(target, mid + 1, high, int_list)) # can search only the upper half of the list, since target is not found, we increase 1 from mid and that is new low 
+    else:
+        return(None) # target is not found in list
+    pass
 
 if __name__ == '__main__':
     max_list_iter(None)
